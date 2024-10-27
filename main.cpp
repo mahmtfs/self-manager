5220e3b0e2b8a0da04e327acfdc42fa8e63c0773
Rinat Mynnakhmetov <rinnik228@gmail.com>
2024-10-26 23:05:33 -0400

    Added custom TabWidget and TabHeader classes

--- main.cpp
+++ main.cpp
@@ -1,21 +1,41 @@
 #include <nanogui/nanogui.h>
+#include <custom/tabwidget/tabwidget.h>
 #include <iostream>
+#include <string>
 
 int main(){
-    bool bvar = false;
-    std::string strval = "something";
-    std::cout << "Hey!\n";
+    nanogui::Vector2i screenSize = nanogui::Vector2i(500, 500);
     nanogui::init();
     {
-    nanogui::Screen *screen = new nanogui::Screen(nanogui::Vector2i(500, 700), "Test");
-    nanogui::FormHelper *gui = new nanogui::FormHelper(screen);
-    nanogui::ref<nanogui::Window> window = gui->addWindow(Eigen::Vector2i(10, 10), "Form helper example");
-    gui->addGroup("Basic types");
-    gui->addVariable("bool", bvar);
-    gui->addVariable("string", strval);
+    nanogui::Screen *screen = new nanogui::Screen(screenSize, "Self-Manager");
+    custom::TabWidget* tabWidget = screen->add<custom::TabWidget>();
+
+    nanogui::Widget* layer = tabWidget->createTab("Routine");
+    layer->setLayout(new nanogui::GroupLayout());
+    layer->add<nanogui::Label>("Routine Tab", "sans-bold");
+
+    layer = tabWidget->createTab("About");
+    layer->setLayout(new nanogui::GroupLayout());
+    layer->add<nanogui::Label>("About Tab", "sans-bold");
+
+    tabWidget->createTab("Placeholder");
+    tabWidget->createTab("+");
+    tabWidget->setActiveTab(0);
+
+    screen->setResizeCallback(
+        [screen](nanogui::Vector2i size){
+            screen->setSize(size);
+            for (nanogui::Widget *child : screen->children()){
+                child->setSize(size);
+                child->performLayout(screen->nvgContext());
+            }
+        }
+    );
     screen->setVisible(true);
     screen->performLayout();
-    window->center();
+    tabWidget->setSize(screenSize);
+    tabWidget->performLayout(screen->nvgContext());
+
     nanogui::mainloop();
     }
     nanogui::shutdown();
